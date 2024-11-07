29f49ccd747a41b5d5c5383c10cfe8de50121e8b
janjanjan312 <1262325196@qq.com>
2024-11-07 00:25:54 +0000

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,25 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def display_form
+
+    render({ :template => "item_templates/backdoor"})
+  end
+
+
+  def create
+
+    i=Item.new
+    i.link_url = params.fetch("link_url_param")
+    i.link_description = params.fetch("link_description")
+    i.thumbnail_url = params.fetch("thumnbnail_url")
+
+    i.save
+
+    # render({ :template => "item_templates/create"})
+    redirect_to("/")
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
+  get("/backdoor", { :controller => "items", :action => "display_form" })
+
+  post("/make_a_new_item", { :controller => "items", :action => "create" })
+
   get("/", { :controller => "items", :action => "index" })
+
+
 end
